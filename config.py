c31c9345218831b94ceef5957bff00d468db1159
Arthur <38541241+Arthurdw@users.noreply.github.com>
2019-08-28 07:00:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,21 @@ token = 'XXX_YOURTOKENHERE_XXX'
 # Main server id:
 server_id = 613835285205352490
 
+# Loggin channel id:
+logging_channel = 597146453240184843
+
 # Reaction roles:
                 # (message_id, role_name, emoji) 
 reaction_roles = [(114034353626021889, 'role_name', '😃')]
 
 # Playing {this message}
 rich_message = 'Your Rich Presence Message'
+
+# Date format for logger:
+    # %d = date
+    # %m = month
+    # %Y = year
+    # %H = hour
+    # %M = minute
+    # %S = seconds
+date_format = '%d/%m/%Y | %H:%M:%S'
